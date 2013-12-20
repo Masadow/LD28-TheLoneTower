1f94a24d9259f57b1472497be2912e89972e3f94
Masadow <jimmy.delas@gmail.com>
2013-12-20 17:50:28 +0000

    Add TweenX library Animate trailing circle

--- source/character/Circle.hx
+++ source/character/Circle.hx
@@ -1,9 +1,15 @@
 package character;
+import flixel.effects.FlxSpriteFilter;
 import flixel.FlxSprite;
 import flixel.util.FlxSpriteUtil;
 import flixel.FlxG;
 import flixel.util.loaders.CachedGraphics;
 import flash.display.Graphics;
+import flash.filters.GlowFilter;
+import tweenx909.TweenX;
+import tweenx909.EaseX;
+
+using tweenx909.ChainX;
 
 /**
  * ...
@@ -12,18 +18,18 @@ import flash.display.Graphics;
 class Circle extends FlxSprite
 {
 	private var _timeleft : Float;
+	private var _glowFilter : GlowFilter;
+	private var _spriteFilter : FlxSpriteFilter;
 	
 	public static function initGraphic()
 	{
 		//Prepare graphic
 		var graphic : CachedGraphics = FlxG.bitmap.create(16, 16, 0x0, true, "circle");
 		FlxSpriteUtil.flashGfx.clear();
-		FlxSpriteUtil.flashGfx.beginFill(0xFFFFFFFF);
-		FlxSpriteUtil.flashGfx.lineStyle(2, 0xFFFFFF, 0.5);
-		FlxSpriteUtil.flashGfx.drawCircle(8, 8, 8);
+		FlxSpriteUtil.flashGfx.beginFill(0xFFFFFF);
+		FlxSpriteUtil.flashGfx.drawCircle(8, 8, 5);
 		FlxSpriteUtil.flashGfx.endFill();
 		graphic.bitmap.draw(FlxSpriteUtil.flashGfxSprite);
-//		FlxSpriteUtil.drawCircle(this, 8, 8, 8, 0x0, { color: 0x5FFFFFFF } );
 	}
 
 	public function new(MakeGraphic : Bool = true) 
@@ -31,6 +37,15 @@ class Circle extends FlxSprite
 		super();
 
 		loadGraphic("circle");
+		
+		//Set glowing
+		_glowFilter = new GlowFilter(0xFFFFFF, 1, 2, 2, 2, 10, false, true);
+		_spriteFilter = new FlxSpriteFilter(this, 16, 16);
+		_spriteFilter.addFilter(_glowFilter);
+//		_spriteFilter.applyFilters();
+
+		//Actuate.tween(filter2, 1, { blurX:4, blurY:4 } ).repeat().reflect().onUpdate(updateFilter, [spr2Filter]).ease(Linear.easeNone);
+		//Actuate.pause(filter2);
 		
 		visible = false;
 	}
@@ -40,12 +55,18 @@ class Circle extends FlxSprite
 		y = Y;
 		visible = true;
 		_timeleft = 0.5; //In seconds
+		
+		//Reset glow and play anim
+		_glowFilter.blurX = 2;
+		_glowFilter.blurY = 2;
+		TweenX.to(_glowFilter).blurX(0).blurY(0).time(_timeleft).ease(EaseX.expoInOut);
 	}
 	
 	override public function update(): Void
 	{
 		super.update();
 
+		_spriteFilter.applyFilters();
 		if (_timeleft > 0 && (_timeleft -= FlxG.elapsed) < 0)
 		{
 			visible = false;
